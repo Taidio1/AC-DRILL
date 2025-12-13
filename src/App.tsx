76e973ab4029b664fe77c72cc055d28637774976
Kacper <kkacper15@gmail.com>
2025-12-13 17:17:25 +0100

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/AC-DRILL">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
